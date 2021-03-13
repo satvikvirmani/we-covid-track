81b2e3c031dce524071b2edada117bcfe182cc31
Satvik Virmani <virmanisatvik01@gmail.com>
2021-03-13 14:28:33 +0530

    Added Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,26 @@ module.exports = {
         display: `standalone`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-PF89N10G4S", // Google Analytics / GA // Google Ads / Adwords / AW// Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
     }
   ],
 };
