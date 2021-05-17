78cc434bec5c68708c037d656a83aa7905a8dba5
Satvik Virmani <70750314+SatvikVirmani@users.noreply.github.com>
2021-05-17 10:24:04 +0530

    Using normal <a></a> tags for links ✔

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from "react"
-import {Link} from "gatsby"
 import {OutboundLink} from "gatsby-plugin-google-gtag"
 import ThemeToggle from "./ThemeToggle"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
@@ -18,18 +17,18 @@ const Navbar = () => {
                         alt="Covid Track"/>
                 </a>
                 <div className="hidden md:flex flex-row justify-evenly">
-                    <Link
-                        to="/"
-                        className="mx-4 text-base hover:text-purple-500 dark:hover:text-purple-500 font-semibold dark:text-gray-400 grid place-items-center">Home</Link>
-                    <Link
-                        to="/#news"
-                        className="mx-4 text-base hover:text-purple-500 dark:hover:text-purple-500 font-semibold dark:text-gray-400 grid place-items-center">News</Link>
-                    <Link
-                        to="/map"
-                        className="mx-4 text-base hover:text-purple-500 dark:hover:text-purple-500 font-semibold dark:text-gray-400 grid place-items-center">Map</Link>
-                    <Link
-                        to="/developer"
-                        className="mx-4 text-base hover:text-purple-500 dark:hover:text-purple-500 font-semibold dark:text-gray-400 grid place-items-center">Developer</Link>
+                    <a
+                        href="/"
+                        className="mx-4 text-base hover:text-purple-500 dark:hover:text-purple-500 font-semibold dark:text-gray-400 grid place-items-center">Home</a>
+                    <a
+                        href="/#news"
+                        className="mx-4 text-base hover:text-purple-500 dark:hover:text-purple-500 font-semibold dark:text-gray-400 grid place-items-center">News</a>
+                    <a
+                        href="/map"
+                        className="mx-4 text-base hover:text-purple-500 dark:hover:text-purple-500 font-semibold dark:text-gray-400 grid place-items-center">Map</a>
+                    <a
+                        href="/developer"
+                        className="mx-4 text-base hover:text-purple-500 dark:hover:text-purple-500 font-semibold dark:text-gray-400 grid place-items-center">Developer</a>
                     <ThemeToggle/>
                 </div>
                 <button
@@ -61,18 +60,18 @@ const Navbar = () => {
                 className={`${isMobile
                 ? "flex"
                 : "hidden"} md:hidden dark:bg-gray-900 flex-col justify-center items-start py-2 border-b-2 border-gray-400`}>
-                <Link
-                    to="/"
-                    className="w-full h-full text-left text-base dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:text-gray-400 py-2 px-4">Home</Link>
-                <Link
-                    to="/#news"
-                    className="w-full h-full text-left text-base dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:text-gray-400 py-2 px-4">News</Link>
-                <Link
-                    to="/map"
-                    className="w-full h-full text-left text-base dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:text-gray-400 py-2 px-4">Map</Link>
-                <Link
-                    to="/developer"
-                    className="w-full h-full text-left text-base dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:text-gray-400 py-2 px-4">Developer</Link>
+                <a
+                    href="/"
+                    className="w-full h-full text-left text-base dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:text-gray-400 py-2 px-4">Home</a>
+                <a
+                    href="/#news"
+                    className="w-full h-full text-left text-base dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:text-gray-400 py-2 px-4">News</a>
+                <a
+                    href="/map"
+                    className="w-full h-full text-left text-base dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:text-gray-400 py-2 px-4">Map</a>
+                <a
+                    href="/developer"
+                    className="w-full h-full text-left text-base dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:text-gray-400 py-2 px-4">Developer</a>
                 <OutboundLink
                     href="https://github.com/SatvikVirmani/we-covid-track"
                     className="w-full h-full text-left text-base dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:text-gray-500 py-2 px-4">
